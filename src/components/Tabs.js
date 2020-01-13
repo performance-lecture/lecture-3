004311342697610e79728dfa4bc7321558146c5c
hackurity01 <hackurity01@gmail.com>
2020-01-13 21:08:19 +0900

    implement Tabs Component

---
+++ src/components/Tabs.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react'
+
+function Tabs(props) {
+	const [tab, setTab] = useState(0)
+	return (
+		<div className="Tabs">
+			<div className="flex">
+				{props.data.map((item, index) => (
+					<div
+						key={"tab-" + item.title}
+						className={"flex-1 text-center py-2 px-1 font-semibold text-xl hover:bg-blue-100" + (tab === index ? ' bg-blue-200 text-blue-800 border-b-2 border-blue-800' : '')}
+						onClick={() => {setTab(index)}}
+					>
+						{item.title}
+					</div>
+				))}
+			</div>
+			<div className="py-10">
+				{props.data[tab].content}
+			</div>
+		</div>
+	)
+}
+
+export default Tabs
--- src/pages/RidingStylesPage.js
+++ src/pages/RidingStylesPage.js
@@ -1,10 +1,38 @@
 import React from 'react'
+import Tabs from '../components/Tabs'
+import BannerImage from '../components/BannerImage'
+import main_styles from '../assets/main-styles.jpg'
 
 function RidingStylesPage(props) {
+	const DATA = [
+		{
+			title: "댄싱",
+			content: "111릭ㅁㄴ이라ㅓㅁㄴ이ㅏ 미낭리마너이머 ㄴ이라ㅓㅁ니ㅏㅇ리ㅏ먼ㅇㄹ"
+		},
+		{
+			title: "트릭",
+			content: "22릭ㅁㄴ이라ㅓㅁㄴ이ㅏ 미낭리마너이머 ㄴ이라ㅓㅁ니ㅏㅇ리ㅏ먼ㅇㄹ"
+		},
+		{
+			title: "다운힐",
+			content: "3릭ㅁㄴ이라ㅓㅁㄴ이ㅏ 미낭리마너이머 ㄴ이라ㅓㅁ니ㅏㅇ리ㅏ먼ㅇㄹ"
+		},
+		{
+			title: "프리라이딩",
+			content: "444444릭ㅁㄴ이라ㅓㅁㄴ이ㅏ 미낭리마너이머 ㄴ이라ㅓㅁ니ㅏㅇ리ㅏ먼ㅇㄹ"
+		}
+	]
+
 
 	return (
-		<div className={'RidingStylesPage'}>
-			RidingStylesPage
+		<div className="RidingStylesPage -mt-16">
+			<BannerImage
+				text={'Riding Styles'}
+				image={main_styles}
+			/>
+			<div className="container py-16 mx-auto">
+				<Tabs data={DATA}/>
+			</div>
 		</div>
 	)
 }
