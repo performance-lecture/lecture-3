5d6e4b78ddae182c39389c07c17ce2e3f3627eb8
Unknown <hackurity01@gmail.com>
2021-03-03 20:08:10 +0900

    3-7 캐시 최적화

--- server/server.js
+++ server/server.js
@@ -5,16 +5,20 @@ const path = require('path')
 
 
 const header = {
-    setHeaders: (res, path) => {
-        res.setHeader('Cache-Control', 'private, no-cache, no-store, must-revalidate')
-        res.setHeader('Expires', '-1')
-        res.setHeader('Pragma', 'no-cache')
-    },
+  setHeaders: (res, path) => {
+    if(path.endsWith('.html')) {
+      res.setHeader('Cache-Control', 'no-cache')
+    } else if(path.endsWith('.js') || path.endsWith('.css') || path.endsWith('.webp')) {
+      res.setHeader('Cache-Control', 'public, max-age=31536000')
+    } else {
+      res.setHeader('Cache-Control', 'no-store')
+    }
+  },
 }
 
 app.use(express.static(path.join(__dirname, '../build'), header))
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build/index.html'))
+  res.sendFile(path.join(__dirname, '../build/index.html'))
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
