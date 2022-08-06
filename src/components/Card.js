4959e7ad01392c2ba5cf6dccdcbcbdb2b4f79d2f
hackurity01 <hackurity01@gmail.com>
2022-08-06 16:26:32 +0900

    3-4 이미지 사이즈 최적화

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,8 +8,12 @@ function Card(props) {
     const callback = (entries, observer) => {
       entries.forEach(entry => {
         if(entry.isIntersecting) {
-          console.log('is intersecting', entry.target.dataset.src)
-          entry.target.src = entry.target.dataset.src
+          const target = entry.target;
+          const previousSibling = target.previousSibling;
+
+          console.log('is intersecting', entry.target.dataset.src);
+          target.src = target.dataset.src;
+          previousSibling.srcset = previousSibling.dataset.srcset;
           observer.unobserve(entry.target)
         }
       })
@@ -24,7 +28,10 @@ function Card(props) {
 
 	return (
 		<div className="Card text-center">
-			<img data-src={props.image} ref={imgRef}/>
+      <picture>
+        <source data-srcset={props.webp} type="image/webp" />
+        <img data-src={props.image} ref={imgRef} />
+      </picture>
 			<div className="p-5 font-semibold text-gray-700 text-xl md:text-lg lg:text-xl keep-all">
 				{props.children}
 			</div>
