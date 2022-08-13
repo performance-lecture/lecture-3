2fe91cfec50ec5c9ad503d153d6d179bd7850d81
hackurity01 <hackurity01@gmail.com>
2022-08-13 18:20:19 +0900

    3-6 폰트 최적화1 - 폰트 적용 시점 제어하기

--- src/components/BannerVideo.js
+++ src/components/BannerVideo.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import video from '../assets/banner-video.mp4'
 import video_webm from '../assets/_banner-video.webm'
+import FontFaceObserver from 'fontfaceobserver'
+
+const font = new FontFaceObserver('BMYEONSUNG')
 
 function BannerVideo() {
+  const [isFontLoaded, setIsFontLoaded] = useState(false)
+
+  useEffect(() => {
+    font.load(null, 20000).then(function () {
+      console.log('BMYEONSUNG has loaded')
+      setIsFontLoaded(true)
+    })
+  }, [])
+
 	return (
 		<div className="BannerVideo w-full h-screen overflow-hidden relative bg-texture">
 			<div className="absolute h-screen w-full left-1/2">
@@ -15,7 +27,9 @@ function BannerVideo() {
           <source src={video} type="video/mp4" />
         </video>
 			</div>
-			<div className="w-full h-full flex justify-center items-center">
+			<div 
+        className="w-full h-full flex justify-center items-center" 
+        style={{opacity: isFontLoaded ? 1 : 0, transition: 'opacity 0.3s ease'}}>
 				<div className="text-white text-center">
 					<div className="text-6xl leading-none font-semibold">KEEP</div>
 					<div className="text-6xl leading-none font-semibold">CALM</div>
